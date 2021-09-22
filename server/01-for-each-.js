0f75ee01eee144c8ca0ddfdd03c3462d0621e4aa
Ulzahk <ulzahk@gmail.com>
2021-09-22 05:20:08 -0500

    [UPDATED] Practice using forEach method

---
+++ server/01-for-each-.js
@@ -0,0 +1,8 @@
+const letter = ["a", "b", "c"];
+
+for (let index = 0; index < letter.length; index++) {
+  const element = letter[index];
+  console.log("for", element);
+}
+
+letter.forEach(item => console.log("forEach", item));
