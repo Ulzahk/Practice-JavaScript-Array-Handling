5eff4c5a701f5769698855e6069f48b4f397f116
Ulzahk <ulzahk@gmail.com>
2021-09-22 08:22:08 -0500

    [UPDATE] Using flatmap method

---
+++ server/14-flatmap.js
@@ -0,0 +1,44 @@
+const users = [
+  { userId: 1, username: "Tom", attributes: ["Nice", "Cute"] },
+  { userId: 2, username: "Mike", attributes: ["Lovely"] },
+  { userId: 3, username: "Nico", attributes: ["Nice", "Cool"] },
+];
+
+const rta = users.map(user => user.attributes).flat();
+console.log("rta", rta);
+const rta2 = users.flatMap(user => user.attributes);
+console.log("rta2", rta2);
+
+const calendars = {
+  primaryCalendar: [
+    {
+      startDate: new Date(2021, 1, 1, 15),
+      endDate: new Date(2021, 1, 1, 15, 30),
+      title: "Cita 1",
+    },
+    {
+      startDate: new Date(2021, 1, 1, 17),
+      endDate: new Date(2021, 1, 1, 18),
+      title: "Cita 2",
+    },
+  ],
+  secondaryCalendar: [
+    {
+      startDate: new Date(2021, 1, 1, 12),
+      endDate: new Date(2021, 1, 1, 12, 30),
+      title: "Cita 2",
+    },
+    {
+      startDate: new Date(2021, 1, 1, 9),
+      endDate: new Date(2021, 1, 1, 10),
+      title: "Cita 4",
+    },
+  ],
+};
+
+const keys = Object.keys(calendars)
+const rta3 = keys.map(key => calendars[key].flatMap(item => item.startDate)).flat(1);
+console.log("rta3", rta3);
+
+const rta4 = Object.values(calendars).flatMap(item => item.map(date => date.startDate));
+console.log("rta3", rta3);
